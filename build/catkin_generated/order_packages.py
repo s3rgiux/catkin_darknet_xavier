b3e2eece1a61f4d64ea36981ec57656f802910d3
s3rgiux <mechatronomus@hotmail.com>
2021-02-09 18:30:13 +0900

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/xavier/catkin_darknet/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/xavier/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/xavier/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/xavier/catkin_darknet/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/xavier/catkin_darknet/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/xavier/catkin_darknet/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/xavier/catkin_darknet/src:$ROS_PACKAGE_PATH"
